1c456feec00973a135f76bef4b916602d9d84c50
Eragon115 <leipherikson@gmail.com>
2020-10-28 13:25:50 -0500

    Update sockets_server.c
    Addition of enumeration and card struct

--- sockets_server.c
+++ sockets_server.c
@@ -20,6 +20,7 @@ int randomize(struct deck cardDeck)
 	for(i = 0; i < 52; i++)
 		printf("%d ", some_array[i]);
 
+	printf("\n");
 	randperm(some_array, 52);
 
 	printf("\n****************After****************\n");
@@ -30,61 +31,83 @@ int randomize(struct deck cardDeck)
 	return(0);
 }
 
+enum suits{spades,hearts,diamonds,clubs};
+enum cards{ace,two,three,four,five,six,seven,eight,nine,ten,jack,queen,king};
+struct card
+{
+    suit card_suit;
+    value card_value;
+}
+
+struct card parse_card(int the_card)
+{
+    struct card temp_card;
+    temp_card.suit=the_card/13;
+    temp_card.value=the_card % 13;
+    return(temp_card);
+}
+
 int main(int argc, char *argv[])
 {
-        int sfd;
-        char buffer[1024];
-        char *words = "Hello World!";
-        struct sockaddr_in sockIn;
-		struct deck cardDeck;
-        int serPort = atoi(argv[1]);
-            //the user should input a server port as an argument
+	int sfd, i;
+	char buffer[1024];
+	char *words = "Hello World!";
+	struct sockaddr_in sockIn;
+	struct deck cardDeck;
+	int serPort = atoi(argv[1]);
+		//the user should input a server port as an argument
 
-        sfd = socket(AF_INET, SOCK_STREAM, 0);
-        sockIn.sin_family = AF_INET;
-        sockIn.sin_addr.s_addr = INADDR_ANY;
-        sockIn.sin_port = htons(serPort);
+	sfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockIn.sin_family = AF_INET;
+	sockIn.sin_addr.s_addr = INADDR_ANY;
+	sockIn.sin_port = htons(serPort);
 
-        int binding = bind(sfd, (struct sockaddr *) &sockIn, sizeof(sockIn));
-		if(binding < 0){
-            printf("Invalid port binding\n");
-			exit(0);
-        }
+	int binding = bind(sfd, (struct sockaddr *) &sockIn, sizeof(sockIn));
+	if(binding < 0){
+		printf("Invalid port binding\n");
+		exit(0);
+	}
 
-        int listening = listen(sfd, 1);
-        printf("Listening for a message...\n");
-		if(listening < 0){
-            printf("Failed to listen\n");
-			exit(0);
-        }
+	int listening = listen(sfd, 1);
+	printf("Listening for a message...\n");
+	if(listening < 0){
+		printf("Failed to listen\n");
+		exit(0);
+	}
 
-        int accepting = accept(sfd, (struct sockaddr *) &sockIn, (socklen_t *)sizeof(sockIn));
-                //blocking call
-		if(accepting < 0){
-            printf("Connection not accepted\n");
-			exit(0);
-        }
-        printf("Accepted the connection!\n");
+	int accepting = accept(sfd, (struct sockaddr *) &sockIn, (socklen_t *)sizeof(sockIn));
+			//blocking call
+	if(accepting < 0){
+		printf("Connection not accepted\n");
+		exit(0);
+	}
+	printf("Accepted the connection!\n");
 
-        int reading = read(sfd, words, (size_t) sizeof(sockIn));
-        printf("Reading from client...\n");
-        if(reading < 0){
-            printf("Failed to read\n");
-			exit(0);
-        }
-		
-		printf("Received message: %s\n", words);
-		char *searchingFor = "deal";
-		if(strcmp(words, searchingFor) == 0){
-			printf("The deal has begun\n");
-			//send to deal function
-		}
-		else{
-			printf("Please input a valid key word\n");
-			//return to listening loop
-		}
-		
-		
+	int reading = read(sfd, words, (size_t) sizeof(sockIn));
+	printf("Reading from client...\n");
+	if(reading < 0){
+		printf("Failed to read\n");
+		exit(0);
+	}
+	
+	printf("Received message: %s\n", words);
+	char *searchingFor = "deal";
+	if(strcmp(words, searchingFor) == 0){
+		printf("The deal has begun\n");
+		//send to deal function
+	}
+	else{
+		printf("Please input a valid key word\n");
+		//return to listening loop
+	}
+	
+	randomize(cardDeck);
+	for(i = 0; i < 52; i++)
+	{
+		printf("Card %d: %d\n", (i + 1), 0);
+	}
+
+
 }
 
 //echo -n "deal" | nc 172.17.68.82 12121
